f6669e0c2ef2c74ea612615913b7299aa7c7c43d
Gary Moore <gary.moore1@ibm.com>
2021-06-24 15:30:09 +0100

    Fix typo in pipinstall function name
    This issue prevented it from being used as it was called pinstall rather
    than pipinstall.

--- pipinstall.fish
+++ pipinstall.fish
@@ -1,3 +1,3 @@
-function pinstall --description 'Convenience function to python3 -m pip --user'
+function pipinstall --description 'Convenience function to python3 -m pip --user'
 python3 -m pip install --user $argv
 end
