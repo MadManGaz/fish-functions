c95213399c873d2c077a4cc988497ba729fe8fa1
Gary Moore <madmangaz@gmail.com>
2021-09-17 16:47:51 +0100

    prefer snake case

--- brotli_decompress.fish
+++ brotli_decompress.fish
@@ -1,4 +1,4 @@
-function brotli-decompress
+function brotli_decompress
     set --local current_file $argv[1]
     brotli -d $current_file.tar.br
     tar -xvf $current_file.tar
--- brotli_decompress_entire_dir.fish
+++ brotli_decompress_entire_dir.fish
@@ -1,4 +1,4 @@
-function brotli-decompress-entire-dir
+function brotli_decompress_entire_dir
     for file in *
         set --local file_no_extension (string split . $file)[1]
         brotli-decompress $file_no_extension &
