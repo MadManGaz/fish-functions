2331da82b194aa746864dc963a65efdc5e08a2d5
Gary Moore <madmangaz@gmail.com>
2022-07-29 13:48:54 +0100

    remove strip and brotli_decompress

--- brotli_decompress.fish
+++
@@ -1,6 +0,0 @@
-function brotli_decompress
-    set --local current_file $argv[1]
-    brotli -d $current_file.tar.br
-    tar -xvf $current_file.tar
-    rm $current_file.tar
-end
--- strip.fish
+++
@@ -1,3 +0,0 @@
-function strip --description llvm-strip
-/usr/local/Cellar/llvm/14.0.6_1/bin/llvm-strip $argv
-end
